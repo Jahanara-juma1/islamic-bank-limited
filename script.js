98dc62a836d9fe2a8fa5512347d8724c158e19c2
Jahanara-juma1 <jahanarajuma213@gmail.com>
2022-02-12 09:54:38 +0600

    withdraw addad

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//
+// deposit
+let depositValue = document.getElementById("deposit-value");
+let depositOutput = document.getElementById("deposit-output");
+
+//Withdraw
+let withdrawValue = document.getElementById("withdraw-value");
+let withdrawOutput = document.getElementById("withdraw-output");
